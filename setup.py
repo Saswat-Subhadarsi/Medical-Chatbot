1832a42d03650d9ce6a4f4130608de574e05033e
Saswat-Subhadarsi-Pattnaik <saswatpattaniksubhadarsi@gmail.com>
2025-06-01 21:26:40 +0530

    Folder structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name = 'Generative AI project',
+    version =  '0.0.0',
+    author = 'Saswat',
+    author_email='saswatpattaniksubhadarsi@gmail.com',
+    packages=find_packages(),
+    install_requires = []
+)
